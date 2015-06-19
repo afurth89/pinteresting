4cbe3dacf88e5e60fd2ca7b3bfab633c1eeeea91
Andrew Furth <andrew.furth@gmail.com>
2015-06-18 18:21:53 -0700

    Added bootstrap and customizations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get "about" => "pages#about"
+  get "about" => "pages#about" # creates about_path
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
