7a3cdd7d0a7c0c4aca233cd5488991e785cb5c0a
Andrew Furth <andrew.furth@gmail.com>
2015-07-20 19:23:59 -0700

    Add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,7 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+	validates :image, presence: true
+	validates :description, presence: true
 end
